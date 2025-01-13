fc7435eda3c304ab7f90fc760776ccafae855625
Gian Pradipta Gunawan <gian.pradipta@ti.ukdw.ac.id>
2025-01-14 01:50:23 +0700

    feat: add remap for the new multiline selector functionality

--- lua/remap/remap.lua
+++ lua/remap/remap.lua
@@ -15,11 +15,32 @@ vim.keymap.set("v", "<c-d>", function ()
 	require("utils.misc").set_mode("n")
 end)
 
-vim.keymap.set("n", "<c-d>", function ()
-	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("n", true, false, true), "n", false)
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(".", true, false, true), "n", false)
+vim.keymap.set("x", "<c-_>", require("features.comment").comment_toggle)
+vim.keymap.set("x", "<c-d>", function ()
+    local ms = require("features.multiple_selections")
+    local len = require("utils.table").get_length
+    local get_visual_selection_text = require("utils.string").get_visual_selection_text
+    local words = get_visual_selection_text()
+    local word = words[len(words)]
+	require("utils.misc").set_mode("n")
+    ms.get_words_and_highlight_first(word)
 end)
 
-vim.keymap.set("x", "<c-_>", require("features.comment").comment_toggle)
+vim.keymap.set("n", "<c-d>", function ()
+    local ms = require("features.multiple_selections")
+    ms.toggle_hl_next()
+end)
+
+vim.keymap.set("n", "<Space>d", function ()
+    local ms = require("features.multiple_selections")
+    ms.clear_hl_next()
+end)
+
+vim.keymap.set("n", "<Space>c", function ()
+    local ms = require("features.multiple_selections")
+    local len = require("utils.table").get_length
+    local word = vim.fn.input("Change to: ", ms.word_now)
+    ms.change_hl_word(word)
+end)
 
 vim.cmd("set mouse=a")
